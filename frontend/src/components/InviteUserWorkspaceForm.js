c28f4fc8a2f3b9e2d114a3dd7b3d623ac92b976e
Jørgen Bele Reinfjell <jorgen.bele@gmail.com>
2020-03-23 16:39:30 +0100

    fix(frontend:components): InviteUserWorkspaceForm fixes

--- frontend/src/components/InviteUserWorkspaceForm.js
+++ frontend/src/components/InviteUserWorkspaceForm.js
@@ -1,25 +1,22 @@
 import React, { useState } from 'react'
 import { connect } from 'react-redux'
 
-import { Headline, HelperText } from 'react-native-paper'
-import { View, TextInput, Button } from 'react-native'
+import { Headline, Button, TextInput } from 'react-native-paper'
+import { View } from 'react-native'
 
 import { inviteMemberWorkspace } from '../api'
 
-function InviteUserWorkspaceForm ({ navigation, route, inviteMemberWorkspace, ...props }) {
+function InviteUserWorkspaceForm ({
+  navigation, route, inviteMemberWorkspace, ...props
+}) {
   const [username, setUsername] = useState('')
 
-  const { added } = props
-  const myWorkspaceID = ''
-  const [pendingInvites, setPendingInvites] = useState([[]])
+  const { workspaceId } = route.params
 
-  const handleToggleMemberAdd = (username) => {
-    inviteMemberWorkspace(myWorkspaceID, username)
-    setPendingInvites(prevPendingInvites => {
-      return {...prevPendingInvites, [myWorkspaceID, username] }
-    })
-    // Do something when members are added to the workspace.
-    // use the workspace ID i am in. Get that from props?
+  const isValidUsername = (username) => username !== ''
+
+  const handleInviteUser = (username) => {
+    inviteMemberWorkspace(workspaceId, username)
   }
 
   return (
@@ -36,37 +33,17 @@ function InviteUserWorkspaceForm ({ navigation, route, inviteMemberWorkspace, ..
 
       <Button
         mode='contained' onPress={() => {
-          console.log(username)
-          handleToggleMemberAdd(username)
-          console.log('Pressed')
+          if (!isValidUsername(username)) return
+          handleInviteUser(username)
         }}
       >
         Add user
       </Button>
-      <HelperText
-        type='info'
-        visible={added}
-        style={{ fontSize: 25, color: '#BA8CDF', textAlign: 'center' }}
-      >
-          You successfully added this user!
-      </HelperText>
-      <HelperText
-        type='info'
-        visible={!added && username !== ''}
-        style={{ fontSize: 25, color: '#BA8CDF', textAlign: 'center' }}
-      >
-          Not a valid user!
-      </HelperText>
-
     </View>
   )
 }
 
-const mapStateToProps = state => ({
-// have to take in members as prop
-  added: state.addMemberWorkspacesState
-
-})
+const mapStateToProps = state => ({ })
 const mapDispatchToProps = { inviteMemberWorkspace }
 
 const ConnectedWorkspacesCreateForm = connect(mapStateToProps, mapDispatchToProps)(InviteUserWorkspaceForm)
